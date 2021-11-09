c329138180b06e506e74883c8545a39e95c0fd13
JosephWong <joseph_wong1993@foxmail.com>
2021-11-09 16:09:00 +0800

    前端展示与操作之登录页与注册页

--- 博学谷/TypeScript入门到精通 助力晋级全栈/front_end/src/views/Login.vue
+++ 博学谷/TypeScript入门到精通 助力晋级全栈/front_end/src/views/Login.vue
@@ -1,5 +1,63 @@
 <template>
-  <div class="about">
-    <h1>用户登录</h1>
+  <div class="root">
+    <h1>订单管理系统</h1>
+    <el-form status-icon label-width="100px" class="demo-ruleForm">
+      <el-form-item label="用户名">
+        <el-input type="text" v-model="username" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">登 录</el-button>
+        <p/>
+        <el-link type="primary" href="/register">注册</el-link>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component'
+
+@Component
+export default class App extends Vue {
+  username = '';
+  password = '';
+
+  submitForm(): void {
+    if (!this.username.trim()) {
+      console.log("请输入用户名");
+      this.$message({
+        message: '请输入用户名',
+        type: 'error'
+      });
+      return;
+    }
+    if (!this.password.trim()) {
+      console.log("请输入登录密码");
+      this.$message({
+        message: '请输入登录密码',
+        type: 'error'
+      });
+      return;
+    }
+
+    //TODO：提交到api
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  padding-left: 50px;
+}
+
+.root {
+  width: auto;
+  max-width: 620px;
+  margin: auto;
+  padding-top: 200px;
+}
+</style>
