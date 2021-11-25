25c9366dfd0284045a1ea11f41e217efc6a08b58
Joseph Wong <joseph9312@qq.com>
2021-11-25 20:39:39 +0800

    4.2.2 在Vue中实现商品列表功能之组件与布局（上）

--- 博学谷/TypeScript入门到精通 助力晋级全栈/vue_pro/src/views/Home.vue
+++ 博学谷/TypeScript入门到精通 助力晋级全栈/vue_pro/src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <router-view/>
+    <Layout>
+      <header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <MenuItem name="1">
+            <Icon type="ios-paper"/>
+            商品列表
+          </MenuItem>
+        </Menu>
+      </header>
+      <content>
+        <router-view/>
+      </content>
+    </Layout>
   </div>
 </template>
--- 博学谷/TypeScript入门到精通 助力晋级全栈/vue_pro/src/views/Item.vue
+++ 博学谷/TypeScript入门到精通 助力晋级全栈/vue_pro/src/views/Item.vue
@@ -1,5 +1,139 @@
 <template>
-  <div class="about">
-    <h1>商品列表</h1>
+  <div class="item_list">
+    <h1 style="padding-top: 50px;">商品列表</h1>
+    <Row type="flex" justify="start" style="padding-top:20px;">
+      <Col span="8"></Col>
+      <Col span="8">
+        <Input search placeholder="Enter something..."/>
+      </Col>
+      <Col span="4">
+        <Select v-model="model1" style="width:200px">
+          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
+      </Col>
+      <Col span="4">
+
+      </Col>
+    </Row>
+
+    <div class="main_area">
+      <ul>
+        <li style="width: 350px;padding: 5px">
+          <Card style="width:345px">
+            <div style="text-align:center">
+              <img src="https://file.iviewui.com/dist/2ecd3b0452aa197097d5131afacab7b8.png">
+              <div>
+                <span style="font-size: 110%;">小米电视4S液晶平板电视</span>
+                <div>
+                  <span style="color: red;">4599.00元/台</span>
+                  <Button type="text" style="color: #2d8cf0;">加入购物车</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </li>
+        <li style="width: 350px;padding: 5px">
+          <Card style="width:345px">
+            <div style="text-align:center">
+              <img src="https://file.iviewui.com/dist/2ecd3b0452aa197097d5131afacab7b8.png">
+              <div>
+                <span style="font-size: 110%;">小米电视4S液晶平板电视</span>
+                <div>
+                  <span style="color: red;">4599.00元/台</span>
+                  <Button type="text" style="color: #2d8cf0;">加入购物车</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </li>
+        <li style="width: 350px;padding: 5px">
+          <Card style="width:345px">
+            <div style="text-align:center">
+              <img src="https://file.iviewui.com/dist/2ecd3b0452aa197097d5131afacab7b8.png">
+              <div>
+                <span style="font-size: 110%;">小米电视4S液晶平板电视</span>
+                <div>
+                  <span style="color: red;">4599.00元/台</span>
+                  <Button type="text" style="color: #2d8cf0;">加入购物车</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </li>
+        <li style="width: 350px;padding: 5px">
+          <Card style="width:345px">
+            <div style="text-align:center">
+              <img src="https://file.iviewui.com/dist/2ecd3b0452aa197097d5131afacab7b8.png">
+              <div>
+                <span style="font-size: 110%;">小米电视4S液晶平板电视</span>
+                <div>
+                  <span style="color: red;">4599.00元/台</span>
+                  <Button type="text" style="color: #2d8cf0;">加入购物车</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </li>
+        <li style="width: 350px;padding: 5px">
+          <Card style="width:345px">
+            <div style="text-align:center">
+              <img src="https://file.iviewui.com/dist/2ecd3b0452aa197097d5131afacab7b8.png">
+              <div>
+                <span style="font-size: 110%;">小米电视4S液晶平板电视</span>
+                <div>
+                  <span style="color: red;">4599.00元/台</span>
+                  <Button type="text" style="color: #2d8cf0;">加入购物车</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </li>
+        <li style="width: 350px;padding: 5px">
+          <Card style="width:345px">
+            <div style="text-align:center">
+              <img src="https://file.iviewui.com/dist/2ecd3b0452aa197097d5131afacab7b8.png">
+              <div>
+                <span style="font-size: 110%;">小米电视4S液晶平板电视</span>
+                <div>
+                  <span style="color: red;">4599.00元/台</span>
+                  <Button type="text" style="color: #2d8cf0;">加入购物车</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </li>
+        <li style="width: 350px;padding: 5px">
+          <Card style="width:345px">
+            <div style="text-align:center">
+              <img src="https://file.iviewui.com/dist/2ecd3b0452aa197097d5131afacab7b8.png">
+              <div>
+                <span style="font-size: 110%;">小米电视4S液晶平板电视</span>
+                <div>
+                  <span style="color: red;">4599.00元/台</span>
+                  <Button type="text" style="color: #2d8cf0;">加入购物车</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<style scoped>
+li {
+  list-style: none;
+  padding: 0;
+  margin: 0 auto;
+  float: left;
+  display: block;
+}
+
+.main_area {
+  width: 75%;
+  display: flex;
+  padding-top: 20px;
+  margin: 0 auto;
+}
+</style>
