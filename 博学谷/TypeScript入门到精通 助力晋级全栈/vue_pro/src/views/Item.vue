6031c98f51a7d03cf9a05393ed8a2ef83a8b39a4
Joseph Wong <joseph9312@qq.com>
2021-11-26 18:01:10 +0800

    4.2.3 在Vue中实现商品列表功能之事件逻辑

--- 博学谷/TypeScript入门到精通 助力晋级全栈/vue_pro/src/views/Item.vue
+++ 博学谷/TypeScript入门到精通 助力晋级全栈/vue_pro/src/views/Item.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="item_list">
-    <h1 style="padding-top: 50px;">商品列表</h1>
+    <h1 style="padding-top: 50px;">商品信息列表</h1>
     <Row type="flex" justify="start" style="padding-top:20px;">
       <Col span="8"></Col>
       <Col span="8">
-        <Input search placeholder="Enter something..."/>
+        <Input search v-model="keyWord" @click.native="search" placeholder="请输入商品关键字匹配..."/>
       </Col>
       <Col span="4">
-        <Select v-model="model1" style="width:200px">
-          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        <Select v-model="orderVal" style="width:150px" clearable @on-change="search" @on-clear="search">
+          <Option v-for="item in optionList" :value="item.value" :key="item.value" style="padding-left: 10px;">
+            {{ item.label }}
+          </Option>
         </Select>
       </Col>
       <Col span="4">
@@ -26,7 +28,7 @@
                 <span style="font-size: 110%;">小米电视4S液晶平板电视</span>
                 <div>
                   <span style="color: red;">4599.00元/台</span>
-                  <Button type="text" style="color: #2d8cf0;">加入购物车</Button>
+                  <Button type="text" style="color: #2d8cf0;" @click="addToCart">加入购物车</Button>
                 </div>
               </div>
             </div>
@@ -120,6 +122,38 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class App extends Vue {
+  itemList = [];
+  keyWord = "";
+  orderVal = "";
+  optionList = [
+    {
+      label: "价格由低到高",
+      value: 1
+    },
+    {
+      label: "价格由高到低",
+      value: -1
+    }
+  ];
+
+  search() {
+    console.log(this.keyWord);
+  }
+
+  addToCart() {
+    this.$Message.success({
+      content: '已加入您的购物车'
+    });
+  }
+}
+</script>
 
 <style scoped>
 li {
