bfb031235affa58bea8a0d08238758924a648310
JosephWong <joseph_wong1993@foxmail.com>
2021-12-20 22:43:57 +0800

    study webpack

---
+++ 官方文档-Webpack/指南/03-管理输出/webpack-demo/src/file.d.ts
@@ -0,0 +1,20 @@
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpg";
+declare module "*.gif";
+
+declare module "*.woff";
+declare module "*.woff2";
+declare module "*.eot";
+declare module "*.ttf";
+declare module "*.otf";
+
+declare module "*.csv";
+declare module "*.tsv";
+
+declare module "*.xml";
+
+declare module "*.toml";
+declare module "*.yaml";
+declare module "*.json5";
+
--- 官方文档-Webpack/指南/03-管理输出/webpack-demo/webpack.config.js
+++ 官方文档-Webpack/指南/03-管理输出/webpack-demo/webpack.config.js
@@ -1,20 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     entry: {
-        app: "./src/index.js",
+        index: "./src/index.js",
         print: "./src/print.js",
     },
     plugins: [
-        new CleanWebpackPlugin(["dist"]),
         new HtmlWebpackPlugin({
-            title: "Output Management"
-        })
+            title: "管理输出"
+        }),
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
 }
