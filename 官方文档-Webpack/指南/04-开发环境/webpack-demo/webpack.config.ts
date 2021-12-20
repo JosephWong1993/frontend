d08ba63f7829e32cb669c11d16e7b72ed5612967
JosephWong <joseph_wong1993@foxmail.com>
2021-12-20 23:14:42 +0800

    study webpack

--- 官方文档-Webpack/指南/04-开发环境/webpack-demo/webpack.config.ts
+++ 官方文档-Webpack/指南/04-开发环境/webpack-demo/webpack.config.ts
@@ -1,25 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
+    mode: "development",
     entry: {
         app: "./src/index.js",
         print: "./src/print.js",
     },
     devtool: "inline-source-map",
     devServer: {
-        contentBase: "./dist"
+        static: "./dist"
     },
     plugins: [
-        new CleanWebpackPlugin(["dist"]),
         new HtmlWebpackPlugin({
-            title: "Output Management"
-        })
+            title: "Development",
+        }),
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, "dist"),
+        clean: true,
         publicPath: "/",
     },
 }
