b7b94af946c7528dd268b6b4fffe9367e90be263
JosephWong <joseph_wong1993@foxmail.com>
2021-12-20 23:57:56 +0800

    study webpack

--- 官方文档-Webpack/指南/06-缓存/webpack-demo/webpack.config.js
+++ 官方文档-Webpack/指南/06-缓存/webpack-demo/webpack.config.js
@@ -1,24 +1,22 @@
 const path = require('path');
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     entry: {
         index: "./src/index.js",
     },
     plugins: [
-        new CleanWebpackPlugin(["dist"]),
         new HtmlWebpackPlugin({
             title: "Caching"
         }),
-        new webpack.HashedModuleIdsPlugin(),
     ],
     output: {
-        filename: '[name].[chunkhash].js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
     optimization: {
+        moduleIds: 'deterministic',
         runtimeChunk: "single",
         splitChunks: {
             cacheGroups: {
