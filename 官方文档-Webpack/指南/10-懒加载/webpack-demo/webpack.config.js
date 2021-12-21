073d0c8b8cf2b13a3a9074e3ef2eb9fac3188835
Joseph Wong <joseph9312@qq.com>
2021-12-21 17:20:02 +0800

    study webpack

--- 官方文档-Webpack/指南/10-懒加载/webpack-demo/webpack.config.js
+++ 官方文档-Webpack/指南/10-懒加载/webpack-demo/webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     entry: {
         index: "./src/index.js",
     },
     plugins: [
-        new CleanWebpackPlugin(["dist"]),
         new HtmlWebpackPlugin({
             title: "Code Splitting"
         })
@@ -16,5 +14,6 @@ module.exports = {
         filename: '[name].bundle.js',
         chunkFilename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
 };
