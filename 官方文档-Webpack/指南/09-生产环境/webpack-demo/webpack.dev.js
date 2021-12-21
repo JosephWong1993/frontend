ae72ed3d8bedf2a401520cf32960c648d3663c6d
Joseph Wong <joseph9312@qq.com>
2021-12-21 17:00:47 +0800

    study webpack

--- 官方文档-Webpack/指南/09-生产环境/webpack-demo/webpack.dev.js
+++ 官方文档-Webpack/指南/09-生产环境/webpack-demo/webpack.dev.js
@@ -1,9 +1,10 @@
-const merge = require("webpack-merge");
+const {merge} = require("webpack-merge");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
+    mode: "development",
     devtool: "inline-source-map",
     devServer: {
-        contentBase: "./dist"
-    }
+        static: "./dist"
+    },
 });
--- 官方文档-Webpack/指南/09-生产环境/webpack-demo/webpack.prod.js
+++ 官方文档-Webpack/指南/09-生产环境/webpack-demo/webpack.prod.js
@@ -1,16 +1,7 @@
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const {merge} = require("webpack-merge");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
-    devtool: "source-map",
-    plugins: [
-        new UglifyJSPlugin({
-            sourceMap: true
-        }),
-        new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify("production")
-        })
-    ]
+    mode: "production",
+    devtool: "source-map"
 });
