14e4a78c36f56806027bd1d314c9558209cd757d
Joseph Wong <joseph9312@qq.com>
2021-12-21 16:32:55 +0800

    study webpack

--- 官方文档-Webpack/指南/08-tree_shaking/webpack-demo/src/index.ts
+++ 官方文档-Webpack/指南/08-tree_shaking/webpack-demo/src/index.ts
@@ -1,8 +1,7 @@
-import { cube } from "./math"
+import {cube} from "./math"
 
 function component() {
-    var element = document.createElement('pre');
-
+    const element = document.createElement('pre');
     element.innerHTML = [
         "Hello webpack!",
         "5 cubed is equal to " + cube(5)
---
+++ 官方文档-Webpack/指南/08-tree_shaking/webpack-demo/src/math.ts
@@ -0,0 +1,6 @@
+export function square(x: number) {
+    return x * x;
+}
+export function cube(x: number) {
+    return x * x * x;
+}
