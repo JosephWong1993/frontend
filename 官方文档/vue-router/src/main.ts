8735708fb54913b619f6c5c7e79742a6e460643a
JosephWong <joseph_wong1993@foxmail.com>
2021-12-24 02:09:59 +0800

    study vue-router

--- 官方文档/vue-router/src/main.ts
+++ 官方文档/vue-router/src/main.ts
@@ -1,9 +1,35 @@
-import Vue from "vue";
+import Vue, {CreateElement} from "vue";
+import VueRouter from 'vue-router';
 import App from "./App.vue";
-import "./styles.css"
+import "./styles.css";
 
-new Vue({
-    render: function (h) {
-        return h(App)
+Vue.use(VueRouter);
+
+const Foo = Vue.extend({
+    render: (h: CreateElement) => {
+        return h("div", "foo");
     },
-}).$mount("#app")
+});
+const Bar = Vue.extend({
+    render: (h: CreateElement) => {
+        return h("div", "bar");
+    },
+});
+
+const routes = [
+    {path: '/foo', component: Foo},
+    {path: '/bar', component: Bar}
+];
+
+const router = new VueRouter({
+    routes // (缩写) 相当于 routes: routes
+})
+
+const indexVue = Vue.extend({
+    render: (h) => {
+        return h(App);
+    },
+    router,
+});
+
+new indexVue().$mount("#app");
