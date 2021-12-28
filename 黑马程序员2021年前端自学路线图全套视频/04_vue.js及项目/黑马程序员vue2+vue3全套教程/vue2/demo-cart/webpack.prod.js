8cc11dfb5b9e25477dd7c2d150a16fa5f5bfed3c
Joseph Wong <joseph9312@qq.com>
2021-12-28 16:46:48 +0800

    study vue2.0

---
+++ 黑马程序员2021年前端自学路线图全套视频/04_vue.js及项目/黑马程序员vue2+vue3全套教程/vue2/demo-cart/webpack.prod.js
@@ -0,0 +1,50 @@
+const {merge} = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "production",
+    devtool: "nosources-source-map",
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/i,
+                include: path.resolve(__dirname, 'src'),
+                loader: "ts-loader",
+                options: {appendTsSuffixTo: [/\.vue$/i]},
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ]
+    }
+});
