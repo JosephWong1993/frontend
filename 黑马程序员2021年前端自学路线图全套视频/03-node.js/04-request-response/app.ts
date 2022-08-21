2f5ffeeb669c307845bdd00c5d07e89dd5a551e5
joseph <joseph9312@qq.com>
2022-08-22 02:03:04 +0800

    study node.js response

--- 黑马程序员2021年前端自学路线图全套视频/03-node.js/04-request-response/app.ts
+++ 黑马程序员2021年前端自学路线图全套视频/03-node.js/04-request-response/app.ts
@@ -15,8 +15,16 @@ http.createServer(function (req, res) {
     // console.log(req.method);
 
     // url 获取这次请求的路径（获取请求报文中的路径，不包含主机名称、端口号、协议）
-    console.log(req.url);
-    res.end('over');
+    // console.log(req.url);
+    // res.end('over');
+
+    // res.write()
+    res.write('Hello world！你好，世界！！！');
+
+    // 每个请求都必须要调用的一个方法 res.end()
+    // 结束响应（请求）
+    // 告诉服务器该响应的报文头，报文体等待全部响应完毕了，可以考虑本次响应结束
+    res.end();
 }).listen(49091, function () {
     console.log('http://localhost:49091');
 });
