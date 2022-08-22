f666ad2b82a121ac5a92a92f387653cba67fb1bc
joseph <joseph9312@qq.com>
2022-08-23 00:13:42 +0800

    study node.js response

--- 黑马程序员2021年前端自学路线图全套视频/03-node.js/04-request-response/app.ts
+++ 黑马程序员2021年前端自学路线图全套视频/03-node.js/04-request-response/app.ts
@@ -18,13 +18,39 @@ http.createServer(function (req, res) {
     // console.log(req.url);
     // res.end('over');
 
+    // res.statusCode = 404;
+    // res.statusMessage = 'Not Found';
+    // res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+    // 通过res.writeHead()来实现
+    // res.writeHead(404, 'Not Found', {
+    //     'Content-Type': 'text/plain;charset=utf-8'
+    // });
+    res.writeHead(200, 'OK', {
+        'Content-Type': 'text/plain;charset=utf-8'
+    });
+
     // res.write()
     res.write('Hello world！你好，世界！！！');
 
     // 每个请求都必须要调用的一个方法 res.end()
     // 结束响应（请求）
     // 告诉服务器该响应的报文头，报文体等待全部响应完毕了，可以考虑本次响应结束
+    // res.end()要响应数据的话，数据必须是String类型或者是Buffer类型
     res.end();
+
+    // res.setHeader()来设置响应报文头
+    // res.setHeader()要放在res.write()和res.end()之前设置
+    // 因为即便我们不设置响应报文头，系统也会默认有响应报文头，并且默认发送给浏览器，当已经发送过响应报文头后，就不能再通过res.setHeader()来再次设置，否则就会报错
+    // res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+
+    // 设置http响应状态码
+    // res.statusCode 设置http响应状态码
+    // res.statusMessage 设置http响应状态码对应的消息
+    // res.statusCode = 404;
+    // res.statusMessage = 'Not Found';
+
+    // res.writeHead() 直接向客户端响应（写入）http响应报文头
+    // 建议在res.write()和res.end()之前调用
 }).listen(49091, function () {
     console.log('http://localhost:49091');
 });
