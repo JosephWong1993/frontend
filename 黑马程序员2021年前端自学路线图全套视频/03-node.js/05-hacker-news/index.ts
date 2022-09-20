0a6e436f0ac961b1394ef87b4179ec7e9099e04c
joseph <joseph9312@qq.com>
2022-09-20 08:41:44 +0800

    fix

--- 黑马程序员2021年前端自学路线图全套视频/03-node.js/05-hacker-news/index.ts
+++ 黑马程序员2021年前端自学路线图全套视频/03-node.js/05-hacker-news/index.ts
@@ -5,10 +5,11 @@ import http from 'http';
 
 http.createServer(function (req, res) {
     // 设计路由
-    // 当用户请求 / 或 /index 时，显示新闻列表
-    // 当用户请求 /item 时，显示新闻详情
-    // 当用户请求 /submit 时，显示添加新闻页面
-    //
+    // 当用户请求 / 或 /index 时，显示新闻列表 - get 请求
+    // 当用户请求 /item 时，显示新闻详情 - get 请求
+    // 当用户请求 /submit 时，显示添加新闻页面 - get 请求
+    // 当用户请求 /add 时，将用户提交的新闻保存到 data.json 文件中 - get 请求
+    // 当用户请求 /add 时，将用户提交的新闻保存到 data.json 文件中 - post 请求
 }).listen(49090, function () {
     console.log('http://localhost:49090');
 });
