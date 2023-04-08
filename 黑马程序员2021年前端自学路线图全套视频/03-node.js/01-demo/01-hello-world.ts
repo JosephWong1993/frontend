16cd139158426d96e6d7e6ed80b89e2e2a93f722
joseph <joseph_wong1993@foxmail.com>
2023-04-08 14:24:22 +0800

    fix

--- 黑马程序员2021年前端自学路线图全套视频/03-node.js/01-demo/01-hello-world.ts
+++
@@ -1,7 +0,0 @@
-let m = 10;
-let n = 100;
-function add(x: number, y: number) {
-    return x + y;
-}
-let result = add(m, n);
-console.log(`计算的结果是：${result}`);
--- 黑马程序员2021年前端自学路线图全套视频/03-node.js/05-hacker-news/types/http.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'http' {
-    interface ServerResponse {
-        render: (filename: string) => void;
-    }
-}
