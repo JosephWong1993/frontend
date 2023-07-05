c9a51d67704b90225841706df473bb823a952e94
Joseph <joseph_wong1993@foxmail.com>
2023-07-05 23:55:50 +0800

    study node.js

--- 黑马程序员2021年前端自学路线图全套视频/03-node.js/04/04-01-require-demo/b.ts
+++ 黑马程序员2021年前端自学路线图全套视频/03-node.js/04/04-01-require-demo/b.ts
@@ -7,7 +7,7 @@ const result = add(100, 1000);
 console.log(result);
 
 // return 'hello';
-// module.exports = 'hello';
+// module.exports = 'hello world!';
 // module.exports = 666;
 module.exports = function (msg) {
     console.log(msg);
