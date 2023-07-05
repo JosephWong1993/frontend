9d9a578a3933b52112cfec8a47ca634f513dbfdf
JosephWong <joseph_wong1993@foxmail.com>
2023-07-05 23:46:42 +0800

    study node.js

--- 黑马程序员2021年前端自学路线图全套视频/03-node.js/04/04-01-require-demo/a.ts
+++ 黑马程序员2021年前端自学路线图全套视频/03-node.js/04/04-01-require-demo/a.ts
@@ -1,24 +1,15 @@
 // 加载b.js模块
-require('./b.js');
+// 一个模块，默认被require()加载后，返回的是一个对象{}
+const b = require('./b.js');
+console.log(b);
+b("HHH666")
 
 // 下面这些加载模块都是从缓存里面获取的，所以不会再去之下b.js文件了
 require('./b.js');
 
-require('./b.js');
-
-require('./b.js');
-
-require('./b.js');
-
-require('./b.js');
-
-require('./b.js');
-
-require('./b.js');
-
 // Module
 // module
-console.log(module.paths);
+// console.log(module.paths);
 // [
 //     'E:\\self-taught-frontend\\黑马程序员2021年前端自学路线图全套视频\\03-node.js\\04\\04-01-require-demo\\node_modules',
 //     'E:\\self-taught-frontend\\黑马程序员2021年前端自学路线图全套视频\\03-node.js\\04\\node_modules',
--- 黑马程序员2021年前端自学路线图全套视频/03-node.js/04/04-01-require-demo/b.ts
+++ 黑马程序员2021年前端自学路线图全套视频/03-node.js/04/04-01-require-demo/b.ts
@@ -5,3 +5,10 @@ function add(x: number, y: number): number {
 const result = add(100, 1000);
 
 console.log(result);
+
+// return 'hello';
+// module.exports = 'hello';
+// module.exports = 666;
+module.exports = function (msg) {
+    console.log(msg);
+}
