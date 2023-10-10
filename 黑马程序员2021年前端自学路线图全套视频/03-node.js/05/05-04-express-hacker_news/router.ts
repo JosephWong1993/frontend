7d5c2bf674283b9f3b875d279f4552ded73874e7
JosephWong <joseph_wong1993@foxmail.com>
2023-10-10 22:12:45 +0800

    study express

--- 黑马程序员2021年前端自学路线图全套视频/03-node.js/05/05-04-express-hacker_news/router.ts
+++ 黑马程序员2021年前端自学路线图全套视频/03-node.js/05/05-04-express-hacker_news/router.ts
@@ -2,17 +2,14 @@
 
 // 1. 创建一个 router 对象（router 对象即是一个对象，也是一个函数）
 import express from "express";
+import * as path from "path";
+// 加载业务模块
+const handler = require('./handler.js');
 const router = express.Router();
 
 // 2. 通过 router 对象设置（挂载）路由
-
-router.get('/', function (req, res) {
-    // body...
-});
-
-router.get('/index', function (req, res) {
-    // body...
-});
+router.get('/', handler.index);
+router.get('/index', handler.index);
 
 router.get('/submit', function (req, res) {
     // body...
@@ -29,6 +26,9 @@ router.get('/add', function (req, res) {
 router.post('/add', function (req, res) {
     // body...
 });
+
+// 实现对 resources 文件夹下的内容进行静态资源托管
+router.use('/resources', express.static(path.join(__dirname, 'resources')));
 
 // 3. 返回 router 对象
 module.exports = router;
