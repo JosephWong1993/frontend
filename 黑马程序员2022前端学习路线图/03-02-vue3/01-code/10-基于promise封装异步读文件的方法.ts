539dd7b095d1d11f399998cf323fde307cc76bef
JosephWong <joseph_wong1993@foxmail.com>
2023-10-12 19:29:38 +0800

    study es6,promise

---
+++ 黑马程序员2022前端学习路线图/03-02-vue3/01-code/10-基于promise封装异步读文件的方法.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import * as console from "console";
+
+function getFile(fpath) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(fpath, 'utf8', (err, dataStr) => {
+            if (err) return reject(err);
+            resolve(dataStr);
+        });
+    });
+}
+
+getFile('./files/11.txt')
+    .then(r1 => {
+        console.log(r1);
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
