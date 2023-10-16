ffa161daf8a5dbd42191dd849e4384d33a214e76
JosephWong <joseph_wong1993@foxmail.com>
2023-10-16 20:17:08 +0800

    study webpack

---
+++ 黑马程序员2022前端学习路线图/03-02-vue3/02-code/src/index.ts
@@ -0,0 +1,10 @@
+// 1. 使用 es6 模块化的语法导入 jquery
+import $ from 'jquery';
+import './css/index.css';
+import './css/index.less';
+
+// 2. 实现隔行变色的效果
+$(function () {
+    $('li:odd').css('backgroundColor', 'red');
+    $('li:even').css('backgroundColor', 'blue');
+})
---
+++ 黑马程序员2022前端学习路线图/03-02-vue3/02-code/webpack.config.ts
@@ -0,0 +1,42 @@
+// const path = require('path');
+import path from 'path';
+// 1. 导入插件，得到构造函数
+import HtmlWebpackPlugin from "html-webpack-plugin";
+// 2. 创建插件的实例对象
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+});
+
+module.exports = {
+    mode: 'development', // 'production' // 'development', // mode 用来制定构建模式。可选值有 development 和 production
+    // 指定打包的入口
+    entry: path.join(__dirname, './src/index.js'),
+    // 指定打包的出口
+    output: {
+        // 表示输出文件的存放路径
+        path: path.join(__dirname, './dist'),
+        // 表示输出文件的名称
+        filename: 'bundle.js'
+    },
+    plugins: [htmlWebpackPlugin], // 3. 挂载插件的实例对象
+    devServer: {
+        open: true,
+        host: '127.0.0.1',
+        port: 80
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.less$/,
+                // include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            },
+        ]
+    }
+}
