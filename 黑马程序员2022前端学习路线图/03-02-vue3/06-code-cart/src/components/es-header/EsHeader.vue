69860f0a40673fde3cd9325bce8131a07134a528
joseph <joseph_wong1993@foxmail.com>
2023-12-04 09:10:16 +0800

    study vue3.0

---
+++ 黑马程序员2022前端学习路线图/03-02-vue3/06-code-cart/src/components/es-header/EsHeader.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="header-container" :style="{ backgroundColor: bgcolor, color: color, fontSize: fsize + 'px' }">
+        {{ title }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EsHeader',
+    props: {
+        // 标题内容
+        title: {
+            type: String,
+            default: 'es-header',
+        },
+        // 背景颜色
+        bgcolor: {
+            type: String,
+            default: '#007BFF',
+        },
+        // 文字颜色
+        color: {
+            type: String,
+            default: '#ffffff',
+        },
+        // 文字大小
+        fsize: {
+            type: Number,
+            default: 12,
+        },
+    },
+}
+</script>
+
+<style lang="less" scoped>
+.header-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 45px;
+  text-align: center;
+  line-height: 45px;
+  z-index: 999;
+}
+</style>
