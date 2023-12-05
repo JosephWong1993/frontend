bb21866633cf07103afc00b61d4df3967ba82ee7
joseph <joseph_wong1993@foxmail.com>
2023-12-05 09:55:38 +0800

    study vue3.0

---
+++ 黑马程序员2022前端学习路线图/03-02-vue3/07-code/src/components/04-base-slot/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>App 根组件</h1>
+        <hr/>
+        <!-- 使用组件 -->
+        <my-com>
+            <!--  在使用MyCom组件时，为插槽指定具体的内容  -->
+            <!--            <p>~~~用户自定义的内容~~~</p>-->
+        </my-com>
+    </div>
+</template>
+
+<script>
+// 导入组件
+import MyCom from './MyCom.vue'
+
+export default {
+    name: 'MyApp',
+    // 注册组件
+    components: {
+        MyCom,
+    },
+}
+</script>
+
+<style lang="less" scoped></style>
