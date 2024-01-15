b5a2f135892fdb03c13b6fa76b5edbe2f1717b01
joseph <joseph_wong1993@foxmail.com>
2024-01-15 21:27:16 +0800

    study vue3

--- 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/01-vue3-project/src/components/01_13/test-com.vue
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/01-vue3-project/src/components/01_13/test-com.vue
@@ -1,1 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {ref} from 'vue';
+
+const name = ref('test name');
+const setName = () => {
+    name.value = 'test new name';
+};
+defineExpose({
+    name,
+    setName
+});
+</script>
+<template>
+    <div>我是test组件</div>
+</template>
--- 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/01-vue3-project/src/main.ts
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/01-vue3-project/src/main.ts
@@ -5,7 +5,8 @@ import './assets/main.css';
 import {createApp} from 'vue';
 // import App from './App.vue';
 // import App from './components/01_11/App.vue';
-import App from './components/01_12/App.vue';
+// import App from './components/01_12/App.vue';
+import App from './components/01_13/App.vue';
 
 // 1. 以App作为参数生成一个应用实例对象
 // 2. 挂载到id为app的节点上
