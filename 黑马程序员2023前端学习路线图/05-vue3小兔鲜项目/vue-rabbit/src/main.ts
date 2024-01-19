becf91ff9f1f7744b8ddc6c70f619e4ea492adb7
joseph <joseph_wong1993@foxmail.com>
2024-01-19 19:28:38 +0800

    study vue3

---
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/main.ts
@@ -0,0 +1,22 @@
+import {createApp} from 'vue';
+import {createPinia} from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+// 引入初始化样式文件
+import '@/styles/common.scss';
+
+import {getCategory} from '@/apis/testApi';
+
+// 测试接口函数
+getCategory().then(res => {
+    console.log(res);
+});
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
---
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/router/index.ts
@@ -0,0 +1,36 @@
+// createRouter：创建router实例对象
+// createWebHistory：创建history模式的路由
+import {createRouter, createWebHistory} from 'vue-router';
+
+import Login from '@/views/login/index.vue';
+import Layout from '@/views/layout/index.vue';
+import Home from '@/views/home/index.vue';
+import Category from '@/views/category/index.vue';
+import * as path from 'path';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    // path和component对应关系的位置
+    routes: [
+        {
+            path: '/',
+            component: Layout,
+            children: [
+                {
+                    path: '',
+                    component: Home
+                },
+                {
+                    path: '/category',
+                    component: Category
+                }
+            ]
+        },
+        {
+            path: '/login',
+            component: Login
+        }
+    ]
+});
+
+export default router;
