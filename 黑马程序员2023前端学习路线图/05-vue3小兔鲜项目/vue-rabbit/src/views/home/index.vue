b80c3af6578133ea6e18bd997cdc2ccc4dd3944d
joseph <joseph_wong1993@foxmail.com>
2024-01-19 19:26:36 +0800

    study vue3

---
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/views/home/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <div>
+        我是home页面
+        <div style="height: 1200px"></div>
+        <!-- 二级路由出口 -->
+        <RouterView/>
+    </div>
+</template>
---
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/views/login/index.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <div>我是登录页</div>
+</template>
