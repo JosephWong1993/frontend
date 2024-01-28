856ac04feba350fb85a75a3a87130869ae095018
joseph <joseph_wong1993@foxmail.com>
2024-01-28 21:19:41 +0800

    study vue3

--- 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/router/index.ts
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/router/index.ts
@@ -1,12 +1,11 @@
 // createRouter：创建router实例对象
 // createWebHistory：创建history模式的路由
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import Login from '@/views/login/index.vue';
 import Layout from '@/views/layout/index.vue';
-import Home from '@/views/home/index.vue';
+import Home from '@/views/home/HomeIndex.vue';
 import Category from '@/views/category/index.vue';
-import * as path from 'path';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
