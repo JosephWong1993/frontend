6308ea79fac167d638837b0b66e868fbcf56a103
joseph <joseph_wong1993@foxmail.com>
2024-01-28 22:20:43 +0800

    study vue3

--- 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/main.ts
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/main.ts
@@ -7,36 +7,12 @@ import router from './router';
 // 引入初始化样式文件
 import '@/styles/common.scss';
 
-import { getCategory } from '@/apis/testApi';
-import { useIntersectionObserver } from '@vueuse/core';
-
-// 测试接口函数
-getCategory().then(res => {
-    console.log(res);
-});
+// 引入懒加载指令插件并且注册
+import { lazyPlugin } from '@/directives';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(lazyPlugin);
 app.mount('#app');
-
-// 定义全局指令
-app.directive('img-lazy', {
-    mounted(el, binding) {
-        // el: 指令绑定的那个元素
-        // bingding: binding.value 指令等于号后面绑定的表达式的值 图片url
-        // console.log(el, binding);
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting }]) => {
-                // console.log(isIntersecting)
-                if (isIntersecting) {
-                    // 进入视口区域
-                    el.src = binding.value;
-                }
-            }
-        )
-    }
-})
