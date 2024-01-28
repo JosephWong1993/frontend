5986db2b9fc612ce825ffe7a89f28e46b58ce99b
joseph <joseph_wong1993@foxmail.com>
2024-01-28 19:59:24 +0800

    study vue3

---
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/views/home/components/HomeBanner.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import {getBannerApi} from '@/apis/home.ts';
+import {ref, onMounted} from 'vue';
+
+const bannerList = ref([]);
+const getBanner = async () => {
+    const res = await getBannerApi();
+    console.log(res);
+    bannerList.value = res.result;
+};
+
+onMounted(() => {
+    getBanner();
+});
+</script>
+
+
+<template>
+    <div class="home-banner">
+        <el-carousel height="500px">
+            <el-carousel-item v-for="item in bannerList" :key="item">
+                <img :src="item.imgUrl" alt="">
+            </el-carousel-item>
+        </el-carousel>
+    </div>
+</template>
+
+
+<style lang='scss' scoped>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
--- 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/views/home/index.vue
+++ 黑马程序员2023前端学习路线图/05-vue3小兔鲜项目/vue-rabbit/src/views/home/index.vue
@@ -1,11 +1,17 @@
-<script lang="ts" setup>
-
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
 </script>
+
 <template>
-    <div>
-        我是home页面
-        <div style="height: 1200px"></div>
-        <!-- 二级路由出口 -->
-        <RouterView/>
+    <div class="container">
+        <HomeCategory/>
+        <HomeBanner/>
     </div>
+    <HomeNew/>
+    <HomeHot/>
+    <HomeProduct/>
 </template>
